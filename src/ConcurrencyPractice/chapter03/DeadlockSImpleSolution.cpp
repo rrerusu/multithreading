d538e9e3f2d778b2c996035746f2ab7930da20e5
rrerusu <rrerusu@gmail.com>
2024-06-23 16:08:05 -0500

    Defer locking and use unique_lock

--- src/ConcurrencyPractice/chapter03/DeadlockSImpleSolution.cpp
+++ src/ConcurrencyPractice/chapter03/DeadlockSImpleSolution.cpp
@@ -12,13 +12,16 @@ class X {
         someBigObject someDetail;
         std::mutex accessMutex;
     public:
-        X(someBigObject const & sd) : someDetail(sd) {}
+        X(someBigObject const & sd) : someDetail(sd) {} 
         friend void swap(X & lhs, X & rhs) {
             if(& lhs == & rhs)
                 return;
-            std::lock(lhs.accessMutex, rhs.accessMutex);
-            std::lock_guard<std::mutex> lockLHS(lhs.accessMutex, std::adopt_lock);
-            std::lock_guard<std::mutex> lockRHS(rhs.accessMutex, std::adopt_lock);
+            // std::lock(lhs.accessMutex, rhs.accessMutex);
+            // std::lock_guard<std::mutex> lockLHS(lhs.accessMutex, std::adopt_lock);
+            // std::lock_guard<std::mutex> lockRHS(rhs.accessMutex, std::adopt_lock);
+            std::unique_lock<std::mutex> lhs_lock(lhs.accessMutex, std::defer_lock);
+            std::unique_lock<std::mutex> rhs_lock(rhs.accessMutex, std::defer_lock);
+            std::lock(lhs_lock, rhs_lock);
             swap(lhs.someDetail, rhs.someDetail);
         }
 };
