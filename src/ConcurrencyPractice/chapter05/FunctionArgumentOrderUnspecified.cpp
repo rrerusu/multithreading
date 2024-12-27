32b56fe30447d3e8dca7b40baf50888b63b5545e
rrerusu <rrerusu@gmail.com>
2024-12-26 19:15:13 -0600

    Argument order is unspecified when passing to function

---
+++ src/ConcurrencyPractice/chapter05/FunctionArgumentOrderUnspecified.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void foo(int a, int b) {
+    std::cout << a << ", " << b << std::endl;
+}
+
+int getNum() {
+    static int i = 0;
+    return ++i;
+}
+
+int main() {
+    foo(getNum(), getNum());
+
+    return EXIT_SUCCESS;
+}
