a66984a1b91ccc8c9d4f3bdc4b94c3c92be76180
rrerusu <rrerusu@gmail.com>
2024-12-27 20:48:13 -0600

    Dummy node to store end of queue

--- src/ConcurrencyPractice/chapter6/SimpleLinkedListQueue.h
+++ src/ConcurrencyPractice/chapter6/SimpleLinkedListQueue.h
@@ -4,7 +4,7 @@ template<typename T>
 class Queue {
     private:
         struct Node {
-            T data;
+            std::shared_ptr<T> data;
             std::unique_ptr<Node> next;
 
             Node(T data_): data(std::move(data_)) {}
@@ -13,7 +13,7 @@ class Queue {
         std::unique_ptr<Node> head;
         Node * tail;
     public:
-        Queue() = default;
+        Queue() : head(new Node), tail(head.get()) {}
         Queue(const Queue & other) = noexcept delete;
         Queue(Queue && other) {
             head = std::move(other.head);
@@ -27,22 +27,20 @@ class Queue {
         ~Queue() noexcept = default;
 
         std::shared_ptr<T> try_pop() {
-            if(!head)
+            if(head.get() == tail)
                 return std::shared_ptr<T>();
             
-            std::shared_ptr<T> const result(std::make_shared<T>(std::move(head->data)));
+            std::shared_ptr<T> const result(head->data);
             std::unique_ptr<Node> const oldHead = std::move(head);
             head = std::move(oldHead->next);
             return result;
         }
         void push(T newVal) {
-            std::unique_ptr<Node> p(new Node(std::move(newVal)));
+            std::shared_ptr<T> newData(std::make_shared<T>(std::move(newVal)));
+            std::unique_ptr<Node> p(new Node);
+            tail->data = newData
             Node * const newTail = p.get();
-            if(tail)
-                tail->next = std::move(p);
-            else
-                head = std::move(p);
-            
+            tail->next = std::move(p);
             tail = newTail;
         }
 };
