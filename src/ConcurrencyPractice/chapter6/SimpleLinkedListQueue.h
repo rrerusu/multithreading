fc5ef2b002e6ae4c0da21628ebe579a67b0845d5
rrerusu <rrerusu@gmail.com>
2024-12-27 21:09:17 -0600

    Deal with potential data races, optimizations and syntax errors

--- src/ConcurrencyPractice/chapter6/SimpleLinkedListQueue.h
+++ src/ConcurrencyPractice/chapter6/SimpleLinkedListQueue.h
@@ -1,4 +1,5 @@
 #include <memory>
+#include <mutex>
 
 template<typename T>
 class Queue {
@@ -7,39 +8,64 @@ class Queue {
             std::shared_ptr<T> data;
             std::unique_ptr<Node> next;
 
-            Node(T data_): data(std::move(data_)) {}
+            Node() = default;
+            Node(T data_): data(std::make_shared<T>(std::move(data_))) {}
         };
 
+        std::mutex headTex;
         std::unique_ptr<Node> head;
+        std::mutex tailTex;
         Node * tail;
+
+        Node * getTail() {
+            std::lock_guard<std::mutex> tailLock(tailTex);
+            return tail;
+        }
+        std::unique_ptr<Node> popHead() {
+            std::lock_guard<std::mutex> head_lock(headTex);
+
+            if(head.get() == getTail())
+                return nullptr;
+            
+            std::unique_ptr<Node> oldHead = std::move(head);
+            head = std::move(oldHead->next);
+            return oldHead;
+        }
+
     public:
         Queue() : head(new Node), tail(head.get()) {}
         Queue(const Queue & other) = noexcept delete;
         Queue(Queue && other) {
+            std::lock(other.headTex, other.tailTex);
+            std::lock_guard<std::mutex> otherHeadLock(other.headTex, std::adopt_lock);
+            std::lock_guard<std::mutex> otherTailLock(other.tailTex, std::adopt_lock);
             head = std::move(other.head);
             tail = std::move(other.tail);
         }
         Queue & operator=(const Queue & other) = delete;
         Queue & operator=(Queue && other) noexcept {
+            std::lock(other.headTex, other.tailTex);
+            std::lock_guard<std::mutex> otherHeadLock(other.headTex, std::adopt_lock);
+            std::lock_guard<std::mutex> otherTailLock(other.tailTex, std::adopt_lock);
+            std::lock(headTex, tailTex);
+            std::lock_guard<std::mutex> headLock(headTex, std::adopt_lock);
+            std::lock_guard<std::mutex> tailLock(tailTex, std::adopt_lock);
             head = std::move(other.head);
             tail = std::move(other.tail);
         }
         ~Queue() noexcept = default;
 
         std::shared_ptr<T> try_pop() {
-            if(head.get() == tail)
-                return std::shared_ptr<T>();
-            
-            std::shared_ptr<T> const result(head->data);
-            std::unique_ptr<Node> const oldHead = std::move(head);
-            head = std::move(oldHead->next);
-            return result;
+            std::unique_ptr<Node> oldHead = popHead();
+            return oldHead ? oldHead->data : std::shared_ptr<T>();
         }
         void push(T newVal) {
             std::shared_ptr<T> newData(std::make_shared<T>(std::move(newVal)));
-            std::unique_ptr<Node> p(new Node);
-            tail->data = newData
+            std::unique_ptr<Node> p(new Node());
             Node * const newTail = p.get();
+
+            std::lock_guard<std::mutex> tailLock(tailTex);
+            tail->data = newData;
             tail->next = std::move(p);
             tail = newTail;
         }
