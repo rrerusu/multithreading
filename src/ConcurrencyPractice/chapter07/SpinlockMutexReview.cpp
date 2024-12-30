b07ddf5fe258444d072b9438ed72282f1832ed28
rrerusu <rrerusu@gmail.com>
2024-12-30 17:30:09 -0600

    Spinlock Mutex - non-blocking structure

---
+++ src/ConcurrencyPractice/chapter07/SpinlockMutexReview.cpp
@@ -0,0 +1,15 @@
+#include <atomic>
+#include <mutex>
+
+class SpinlockMutex {
+    private:
+        std::atomic_flag flag;
+    public:
+        SpinlockMutex() : flag(ATOMIC_FLAG_INIT) {}
+        void lock() {
+            while(flag.test_and_set(std::memory_order_acquire));
+        }
+        void unlock() {
+            flag.clear(std::memory_order_release);
+        }
+};
