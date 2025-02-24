68d74f828d84715cf884041fe78f429f4ee81c89
rrerusu <rrerusu@gmail.com>
2025-02-24 14:03:24 -0600

    Delete node, ensure no other nodes are accessing that node at the same time

--- src/ConcurrencyPractice/chapter07/LockFreeStack/LockFreeStack.h
+++ src/ConcurrencyPractice/chapter07/LockFreeStack/LockFreeStack.h
@@ -11,6 +11,48 @@ class LockFreeStack {
             Node(T const & data_) : data(std::make_shared<T>(data_)) {}
         };
 
+        std::atomic<unsigned> threadsInPop;
+        std::atomic<Node * > toBeDeleted;
+
+        static void deleteNodes(Node * nodes) {
+            while(nodes) {
+                Node * next = nodes->next;
+                delete nodes;
+                nodes = next;
+            }
+        }
+
+        void tryReclaim(Node * oldHead) {
+            if(threadsInPop == 1) {
+                Node * nodesToDelete = toBeDeleted.exchange(nullptr);
+                if(!--threadsInPop)
+                    deleteNodes(nodesToDelete);
+                else if(nodesToDelete)
+                    chainPendingNodes(nodesToDelete);
+                delete oldHead;
+            } else {
+                chainPendingNode(oldHead);
+                --threadsInPop;
+            }
+        }
+
+        void chainPendingNodes(Node * nodes) {
+            Node * lastNode = nodes;
+            while(Node * const nextNode = lastNode->next)
+                lastNode = nextNode;
+            chainPendingNodes(nodes, lastNode);
+        }
+
+        // TODO: Check this
+        void chainPendingNodes(Node * firstNode, Node * lastNode) {
+            lastNode->next = toBeDeleted;
+            while(!toBeDeleted.compare_exchange_weak(lastNode->next, firstNode));
+        }
+
+        void chainPendingNode(Node * n) {
+            chainPendingNodes(n, n);
+        }
+
         std::atomic<Node * > head;
     public:
         void push(T const & data) {
@@ -19,8 +61,14 @@ class LockFreeStack {
             while(!head.compare_exchange_weak(newNode->next, newNode));
         }
         std::shared_ptr<T> pop() {
+            ++threadsInPop;
             Node * oldHead = head.load();
             while(oldHead && !head.compare_exchange_weak(oldHead, oldHead->next));
-            return oldHead ? oldHead->data : std::shared_ptr<T>();
+            std::shared_ptr<T> res;
+            if(oldHead)
+                res.swap(oldHead->data);
+            
+            tryReclaim(oldHead);
+            return res;
         }
 };
